86cde62126b9e1c0e14af8462aa8c6251ece5a25
Vítor Paes <vitor.paes@compasso.com.br>
2024-02-25 23:39:09 -0300

    feat: add font-source Comfortaa

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import "@fontsource-variable/comfortaa";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import '@fontsource-variable/comfortaa'
+import { ThemeProvider } from 'styled-components'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import theme from './components/ui/theme'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+)
