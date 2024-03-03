3652f2f0aed63a944986f219a7ca3391d21f67b7
Vítor Paes <vitor.paes@compasso.com.br>
2024-03-03 03:07:27 -0300

    fix: deploy bash command to use pm2

--- bazar-server/bash/deploy.sh
+++ bazar-server/bash/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Processando argumentos passados para o script
 while [ $# -gt 0 ]; do
   if [[ $1 == *=* ]]; then
     declare $(echo $1 | cut -d= -f1)="$(echo $1 | cut -d= -f2-)"
@@ -16,17 +17,18 @@ PUBLIC_DIR="$NESTJS_APP_DIR/public"
 echo "Iniciando o build do React"
 
 cd $REACT_APP_DIR
-# yarn install
-# yarn build --mode=$mode
+npm install
+npm run build --mode=$mode
 
 echo "Copiando arquivos de build para o diretório público do NestJS..."
 mkdir -p $PUBLIC_DIR
 rm -rf $PUBLIC_DIR/*
 cp -R dist/* $PUBLIC_DIR
 
-echo "Iniciando o servidor NestJS..."
-cd ../$NESTJS_APP_DIR
-yarn install
-yarn start
+echo "Iniciando o servidor NestJS com PM2..."
+cd $NESTJS_APP_DIR
+npm install
+
+pm2 restart bazar-server
 
 echo "Deploy completo."
